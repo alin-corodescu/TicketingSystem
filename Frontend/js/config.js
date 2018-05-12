0d800cc6b98d44990255c816d134fa4839fc07ba
CristiHuma96 <cristihuma96@gmail.com>
2018-05-12 19:41:53 +0300

    Login system up and running

---
+++ Frontend/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-1_XR4Bqh33X', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2kqhv8f57o6phi3af0gsri37sc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
